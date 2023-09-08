4a7aff1f534213daf576f7f9ce068054ab612e22
mifa43 <58310225+mifa43@users.noreply.github.com>
2023-09-08 10:28:20 +0200

    Dodavanje app

---
+++ hr_statistic/app/analytics.py
@@ -0,0 +1,243 @@
+import pandas as pd
+from pathlib import Path
+
+class LocateAndLoadData:
+    """### Klasa koja ce da locira svaku excel datoteku iz zadatog direktorijuma i kreira objekat DataFrame
+        :param|protected_all
+        - `self.__path_to_data: str` -> putanja do direktorijuma sa podatcima
+        - `self.__located_data: list` -> lista ge se smesta locirana .xlsx datoteka
+        - `self.pandas_dataframe: list` -> ucitavamo pronadjene datoteke | pubilc
+    """
+
+    def __init__(self) -> None:
+        
+        self.__path_to_data: str = "/home/mifa43/Desktop/hr_statistic/data"
+
+        self.__located_data: list = []
+
+        option: str = None
+
+        self.df = None
+
+        # self.pandas_dataframe: list = []
+
+    def locate_data(self):
+        """### Pronalazimo sve datoteke sa *.xlsx u datom direktorijumu"""
+
+        folder = Path(self.__path_to_data)
+
+        if folder.exists() and folder.is_dir():
+
+            is_file_excel = folder.glob("*.xlsx")
+            
+            for file in is_file_excel:
+
+                self.__located_data.append(file)
+
+    def load_data(self, option: str = None):
+        """### Ucitavanje datoteka i vracanje DataFrame objekta
+            :param
+            - `option: str = None` -> Prima kao parametar kom ili telem
+        """
+
+        # Prolazimo kroz alociranu datoteke
+        for data in self.__located_data:
+
+            # Ako je opcija = Komercijala otvaramo datoteku za kom, moze da bude i Telemarketing
+            if (option is None or option.lower() in str(data).lower()) and data.exists():
+                
+                print(f"Opening the file: {data}")
+                
+                self.df = pd.read_excel(data, 1)
+                
+                return self.df
+        
+        print("File not found or an unexpected error occurred!")
+class CleanData(LocateAndLoadData):
+    """### Ciscenje podataka iz tabela ako je citava kolona NaN brisi prazna polja
+    :param
+        - `self.clean_data: list` -> lista ociscenih pandas DataFrame
+    """
+
+    def __init__(self) -> None:
+
+        super().__init__()
+
+    def clean(self):
+        """### Metoda koja radi iteraciju kroz alocirane excel datoteke  i izbacuje prazna polja"""
+        # Prljavi podatci i varijacije
+        name_variant = {"Lepša": ["Lepša", "lepša ", " lepša", "lePŠA", "Lepša  ", "lepša  "],
+                        "Lepša ZTP": ["lepša ZTP", "Lepša ztp "],
+                        "Nemanja": ["Nemanja ", "nemanja", "nemanja ", "Nemanja "],
+                        "Nemanja Božinovski": ["Nemanja B", "Nemanja B ", "Nemanja B.", "nemanja b", "nemanja B"],
+                        "Goran": ["Goran ", "goran"],
+                        "Mara": ["MARA", "mara"],
+                        "Ana": ["ANA", "ana"],
+                        "Sale": ["Sale", "sale"],
+                        "Milica": ["MIlica", "MILICA", "milica"],
+                        "Tijana Ž.": ["Tijana", "tijana"],
+                        "Peđa": ["peđa", "Peđa"]
+                        }
+
+        # Izvlacimo imena rukovodilaca
+        get_keys = list(name_variant.keys())
+            
+        # Kolika je duzina liste sa imenima rukovodilaca
+        for key in range(len(get_keys)):
+
+            # Prolazimo kroz sve ne ispravne varijacije u name_variant
+            for name in name_variant[get_keys[key]]:
+                
+                # Konvertujemo ime rukovodilca u text i izbacujemo razmake i sve varijacije stavljamo u mala slova i dodajemo vrednost keya
+                self.df.loc[self.df["Rukovodilac"].str.strip().str.lower() == name.lower(), "Rukovodilac"] = get_keys[key]
+
+        self.df.loc[:, "Datum zakazivanja"] = pd.to_datetime(self.df["Datum zakazivanja"], format="%d %B, %Y")
+        
+        self.df["Datum obuke"] = pd.to_datetime(self.df["Datum obuke"], format="%d %B, %Y")
+
+        self.df = self.df.dropna(thresh=4)  
+
+        return self.df
+
+class Analytic(CleanData):
+
+    def __init__(self) -> None:
+
+        super().__init__()
+
+    def filter_by_date(self, rukovodilac: str, start_date: str, end_date: str):
+        """### Pretrazuje po zadatom kriterijumu i vraca dataframe
+        :param
+        - `rukovodilac: str` -> Ime rukovodilca 
+        - `start_date: str` -> Datum od: pocetak range-a
+        - `end_date: str` -> Datum do: kraj range-a
+
+        :return
+        - `DataFrame` -> Daje pandas df za zadate kriterijume
+        """
+        # Formatiranje datuma
+        start_date = pd.to_datetime(start_date, format="%d-%m-%Y")
+        end_date = pd.to_datetime(end_date, format="%d-%m-%Y")
+
+        self.df.loc[:, "Datum zakazivanja"] = pd.to_datetime(self.df["Datum zakazivanja"], format="%d %B, %Y")
+        
+        # Kriterijum pretrage
+        new_df = self.df.loc[(self.df["Rukovodilac"] == rukovodilac) & (self.df["Datum zakazivanja"] >= start_date) & (self.df["Datum zakazivanja"] < end_date) & (self.df["Pojavio se"].str.lower() == "da")]
+        
+        # Grupisanje podataka po "Datum zakazivanja" i brojanje za svaki dan u range
+        counts_by_day = new_df.groupby("Datum zakazivanja")["Datum zakazivanja"].count()
+
+        # Preimenovanje kolona
+
+        return counts_by_day
+    
+    def pie_data(self, start_date: str, end_date: str):
+        # Formatiranje datuma
+        start_date = pd.to_datetime(start_date, format="%d-%m-%Y")
+        end_date = pd.to_datetime(end_date, format="%d-%m-%Y")
+
+        # Izvlačenje svih imena rukovodilaca
+        rukovodilci = self.df["Rukovodilac"].unique()
+
+        self.df.loc[:, "Datum zakazivanja"] = pd.to_datetime(self.df["Datum zakazivanja"], format="%d %B, %Y")
+
+        # Inicijalizacija praznog rečnika za čuvanje rezultata za svakog rukovodioca
+        rezultati = {}
+        rezultati_acepted = {}
+        rezultati_obuka = {}
+        rezultati_declined = {}
+
+        for rukovodilac in rukovodilci:
+            # Kriterijum pretrage za svakog rukovodioca
+            new_df = self.df.loc[
+                (self.df["Rukovodilac"] == rukovodilac) & 
+                (self.df["Datum zakazivanja"] >= start_date) & 
+                (self.df["Datum zakazivanja"] < end_date) & 
+                (self.df["Pojavio se"].str.lower() == "da")
+                ]
+            
+            new_df_accepted = self.df.loc[
+                (self.df["Rukovodilac"] == rukovodilac) & 
+                (self.df["Datum zakazivanja"] >= start_date) & 
+                (self.df["Datum zakazivanja"] < end_date) & 
+                (self.df["Pojavio se"].str.lower() == "da") & 
+                (self.df["Status"].str.lower() == "da")
+                ]
+            
+            new_df_traning = self.df.loc[
+                (self.df["Rukovodilac"] == rukovodilac) & 
+                (self.df["Datum zakazivanja"] >= start_date) & 
+                (self.df["Datum zakazivanja"] < end_date) & 
+                (self.df["Pojavio se"].str.lower() == "da") & 
+                (self.df["Status"].str.lower() == "da") & 
+                (self.df["Datum obuke"].notna())
+                ]
+            
+            new_df_traning_declined = self.df.loc[
+                (self.df["Rukovodilac"] == rukovodilac) & 
+                (self.df["Datum zakazivanja"] >= start_date) & 
+                (self.df["Datum zakazivanja"] < end_date) & 
+                (self.df["Pojavio se"].str.lower() == "da") & 
+                (self.df["Status"].str.lower() == "da") & 
+                (self.df["Datum obuke"].isnull() | (self.df["Datum obuke"] == pd.NaT))
+                ]
+        
+            
+            # Brojanje intervjua za svakog rukovodioca
+            ukupan_broj_intervjua = len(new_df)
+
+            ukupan_broj_primljenih = len(new_df_accepted)
+            
+            ukupan_broj_obuka = len(new_df_traning)
+
+            ukupan_broj_declined = len(new_df_traning_declined)
+
+            if ukupan_broj_intervjua > 0 and ukupan_broj_primljenih > 0 and ukupan_broj_obuka > 0 and ukupan_broj_declined > 0:
+                
+                rezultati[rukovodilac] = ukupan_broj_intervjua
+
+                rezultati_acepted[rukovodilac] = ukupan_broj_primljenih
+
+                rezultati_obuka[rukovodilac] = ukupan_broj_obuka
+
+                rezultati_declined[rukovodilac] = ukupan_broj_declined
+
+        return rezultati, rezultati_acepted, rezultati_obuka, rezultati_declined
+    
+        
+# l = Analytic()
+
+# l.locate_data()
+
+# l.load_data("Telemarketing")
+
+# ch = l.clean()
+
+# ch = l.pie_data("01-01-2023", "30-09-2023")
+
+# num = 0
+# for k,v in zip(ch[2].keys(), ch[2].values()):
+#     x = v
+#     num = num + x
+# print(num)
+# for key in ch:
+#     if ch[key] > 0:
+#         print(f"{key}:{ch[key]}")
+# key = list(ch.keys())
+# value = list(ch.values())
+# print(key, value)
+# ch = l.filter_by_date("Nemanja Božinovski", "01-01-2023", "30-09-2023")
+
+# # # rukovodilac = ch["Rukovodilac"].drop_duplicates()
+# result = []
+# for idx, val in zip(ch.index, ch.values):
+#     result.append([idx, val])
+# # Kreirajte DataFrame
+# df = pd.DataFrame(result, columns=["Datum zakazivanja", "Broj intervjua"])
+
+# # Pretvorite kolonu "Datum" u tip podataka za datume
+# df["Datum zakazivanja"] = pd.to_datetime(df["Datum zakazivanja"])
+
+# # Prikazivanje DataFrame-a
+# print(df["Broj intervjua"].sum())
+
---
+++ hr_statistic/app/main.py
@@ -0,0 +1,277 @@
+import dash
+from dash import html, dcc
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+import numpy as np
+from datetime import date
+from dash_bootstrap_templates import ThemeChangerAIO, template_from_url
+from plotly.subplots import make_subplots
+from analytics import Analytic
+
+dbc_css = (
+    "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.1/dbc.min.css"
+)
+
+# Inicijalizacija Dash-a
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
+
+# Inicijalizacija Dash boostrap tema
+theme_change = ThemeChangerAIO(aio_id="theme", radio_props={"value":dbc.themes.MORPH})
+
+analytic = Analytic()
+
+figPie = make_subplots(
+    rows=1, 
+    cols=4, 
+    subplot_titles=['Odrzanih intervjua', 'Primljenih kandidata', 'Pristali na obuku', 'Odbili obuku'], 
+    specs=[[{'type':'domain'}, {'type':'domain'}, {'type':'domain'}, {'type':'domain'}]]
+    )
+
+app.layout = html.Div([
+
+    dbc.Container([theme_change], className="m-4 dbc"),
+
+    html.H1("Statistika", style={"font-size": "60px", "padding": "30px"}),
+    html.Label("Izaberi podatke za analiziranje:", style={"font-weight": "bold", "padding": "10px"}),
+        dcc.Dropdown(
+        id = "dropdown-column1",
+        options=[
+            {"label": "Komercijala", "value": "Komercijala"},
+            {"label": "Telemarketing", "value": "Telemarketing"}
+        ],
+        style={"color": "black"},
+        className="m-1",
+        value=None,
+        placeholder="Izaberi podatke za analiziranje.."
+    ),
+    html.Label("Izaberi rukovodilca:", style={"font-weight": "bold", "padding": "10px"}),
+    dcc.Dropdown(
+        id="dropdown-column2",
+        options=[],
+        style={"color": "black"},
+        className="m-1",
+        value=None,
+        placeholder="Izaberi rukovodilca.."
+    ),
+    html.Label("Izaberi opseg datuma:", style={"font-weight": "bold", "padding": "10px"}),
+    dcc.DatePickerRange(
+        id="date-picker-range",
+        start_date_placeholder_text="Datum od",
+        end_date_placeholder_text="Datum do",
+        calendar_orientation='vertical',
+        style={"padding": "10px"}
+    ),
+    
+    dcc.Graph(id="line-plot", className="m-4", style={"width": "94.9%"}),
+
+    html.H2("Odnos", style={"font-size": "60px", "padding": "30px"}),
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='pie-graph', className="m-4", style={"width": "90%"}), md=3),
+            dbc.Col(dcc.Graph(id='pie-graph1', className="m-4", style={"width": "90%"}), md=3),
+            dbc.Col(dcc.Graph(id='pie-graph2', className="m-4", style={"width": "90%"}), md=3),
+            dbc.Col(dcc.Graph(id='pie-graph3', className="m-4", style={"width": "90%"}), md=3),
+        ], className="flex-row flex-wrap")
+    ], fluid=True)
+
+
+], style={"display": "flex", "flexDirection": "column","flexWrap": "wrap"}, className="BABA")
+
+
+@app.callback(
+    Output("dropdown-column2", "options"),
+    Input("dropdown-column1", "value")
+)
+def update_drop_down_column2(selected_value):
+    
+    # Alociramo datoteke
+    analytic.locate_data()
+
+    # Biramo koji excel sheet se analizira
+    analytic.load_data(selected_value)
+
+    # Pozivanje cistih podataka
+    rukovodilac = analytic.clean()
+
+    # Postoje samo dve moguce opcije ako input nije onda vrati praznu listu za options
+    if selected_value in ["Komercijala", "Telemarketing"]:
+
+        rukovodilac = rukovodilac[rukovodilac["Rukovodilac"].notna()]["Rukovodilac"].unique()
+
+    else:
+
+        rukovodilac = []
+
+    # Ako postoje imena vracamo opcije u suprotnom []
+    options = [{"label": ruk, "value": ruk} for ruk in rukovodilac]
+
+    return options if options else []
+
+@app.callback(
+    Output("line-plot", "figure"),
+    [
+        Input("dropdown-column1", "value"), 
+        Input("dropdown-column2", "value"), 
+        Input("date-picker-range", "start_date"), 
+        Input("date-picker-range", "end_date"),
+        Input(ThemeChangerAIO.ids.radio("theme"), "value")
+    ]
+)
+def update_line_plot(selected_column1, selected_column2, start_date, end_date, theme):
+
+    # Prikaz praznog graph-a
+    figLine = px.line(template=template_from_url(theme))
+
+    # ZA LINE GRAPH SVE MORA DA BUDE TACNO
+    if selected_column1 is not None and selected_column2 is not None and start_date is not None and end_date is not None:
+
+        # Alociramo datoteke
+        analytic.locate_data()
+
+        # Biramo koji excel sheet se analizira
+        analytic.load_data(selected_column1)
+
+        # Pozivanje cistih podataka
+        analytic.clean()
+
+        # Formatiranje datuma
+        start_date_object = date.fromisoformat(start_date)
+        end_date_object = date.fromisoformat(end_date)
+
+        # Filtriranje po zadatom parametru
+        filtr_data = analytic.filter_by_date(
+            selected_column2, 
+            start_date_object,
+            end_date_object
+            )
+        
+        # Ekstraktujemo vrednosti i pakujemo kao [[idx, val], [idx1, val1], ...]
+        graph_data = [(idx, val) for idx, val in zip(filtr_data.index, filtr_data.values)]
+
+        # Konvertujemo podatke u novi DataFrame
+        df = pd.DataFrame(graph_data, columns=["Datum zakazivanja", "Broj intervjua"])
+        
+        # Kolonu Datum zakazivanja konvertujemo u pd datetime tip
+        df["Datum zakazivanja"] = pd.to_datetime(df["Datum zakazivanja"])
+
+        intervju_sum = df["Broj intervjua"].sum()
+
+        # Ubacujemo varibialne podatke u graph i temu ako je primenjena
+        figLine = px.line(
+            df, 
+            x="Datum zakazivanja", 
+            y="Broj intervjua", 
+            title=f"Grafikon za {selected_column2} | Ukupan broj intervjua je {intervju_sum} za zadati datum: {start_date_object} / {end_date_object}",
+            template=template_from_url(theme),
+            markers=True
+        )
+
+        return figLine
+    
+    # Ako nije ispunjen uslov vrati prazan graph a ako je odabran tema primeni je
+    else:
+    
+        return figLine.update_layout(title="No Data Available",template=template_from_url(theme))
+
+@app.callback(
+    Output('pie-graph', 'figure'),
+    Output('pie-graph1', 'figure'),
+    Output('pie-graph2', 'figure'),
+    Output('pie-graph3', 'figure'),
+    [
+        Input("dropdown-column1", "value"),
+        Input("date-picker-range", "start_date"), 
+        Input("date-picker-range", "end_date"),
+        Input(ThemeChangerAIO.ids.radio("theme"), "value"),
+    ]
+)
+def update_pie_chart(selected_column1, start_date, end_date, theme):
+
+    if selected_column1 is not None and start_date is not None and end_date is not None:
+        
+        # Alociramo datoteke
+        analytic.locate_data()
+
+        # Biramo koji excel sheet se analizira
+        analytic.load_data(selected_column1)
+
+        # Pozivanje cistih podataka
+        analytic.clean()
+
+        # Formatiranje datuma
+        start_date_object = date.fromisoformat(start_date)
+        end_date_object = date.fromisoformat(end_date)
+
+        filtr_data = analytic.pie_data(start_date_object, end_date_object)
+
+        labels = list(filtr_data[0].keys())
+
+        values = list(filtr_data[0].values())
+        values1 = list(filtr_data[1].values())
+        values2 = list(filtr_data[2].values())
+        values3 = list(filtr_data[3].values())
+
+        # Dodajemo podatke za pie graph
+        figPie = px.pie(
+            names=labels, 
+            values=values,
+            title='Ukupan broj intervjua',
+            template=template_from_url(theme),
+        )
+        figPie1 = px.pie(
+            names=labels, 
+            values=values1,
+            title='Ukupan broj primljenih',
+            template=template_from_url(theme),
+        )
+        figPie2 = px.pie(
+            names=labels, 
+            values=values2,
+            title='Pristali na obuku',
+            template=template_from_url(theme),
+        )
+        figPie3 = px.pie(
+            names=labels, 
+            values=values3,
+            title='Odbili obuku',
+            template=template_from_url(theme),
+        )
+
+        return figPie, figPie1, figPie2, figPie3
+    
+    else:
+        # Ako uslov nije zadovoljen vracamo prazan pie graph sa No data porukom 
+        figPie = px.pie(
+            names=["No Data"], 
+            values=[100],
+            title='No Data Available',
+            template=template_from_url(theme),
+        )
+        figPie1 = px.pie(
+            names=["No Data1"], 
+            values=[100],
+            title='No Data Available1',
+            template=template_from_url(theme),
+        )
+        figPie2 = px.pie(
+            names=["No Data1"], 
+            values=[100],
+            title='No Data Available2',
+            template=template_from_url(theme),
+        )
+        figPie3 = px.pie(
+            names=["No Data3"], 
+            values=[100],
+            title='No Data Available3',
+            template=template_from_url(theme),
+        )
+
+        return figPie, figPie1, figPie2, figPie3
+
+# Pokretanje servera
+if __name__ == "__main__":
+
+    app.run_server(debug=True)
