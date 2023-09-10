a957f8d2ded482aae0c624ff85b1f96af97523b4
Mifa43 <mifa43kotez@gmail.com>
2023-09-10 21:59:06 +0200

    dodata metoda za upisivanje i analiziranje podataka za dash tabel

--- hr_statistic/app/analytics.py
+++ hr_statistic/app/analytics.py
@@ -1,6 +1,32 @@
 import pandas as pd
 from pathlib import Path
 import os
+
+def format_str(data):
+    """### Pomocna funkcija koja uzima data set i kreira markdown tabelu
+    :param
+    - `data: DataFrame` -> Zahteva sorted_grouped_data["Rukovodioci dict"]
+
+    :return
+    - `element_list: list` -> Vrca generisane markdown tabele 
+    """
+    element_list = []
+
+    for sublist in data:
+
+        text = """
+| Rukovodilac | Broj ponavljanja |
+| ----------- | ----------- |
+"""
+        for key, value in sublist.items():
+
+            text += f"| {key} | {value} |\n"
+        
+        element_list.append(text)
+    
+    return element_list
+
+
 class LocateAndLoadData:
     """### Klasa koja ce da locira svaku excel datoteku iz zadatog direktorijuma i kreira objekat DataFrame
         :param|protected_all
@@ -10,8 +36,8 @@ class LocateAndLoadData:
     """
 
     def __init__(self) -> None:
-        # local develop path ./hr_statistic/app
-        self.__path_to_data: str = "/opt/render/project/src/hr_statistic/app"
+        # local develop path ./hr_statistic/app | /opt/render/project/src/hr_statistic/app 
+        self.__path_to_data: str = "/opt/render/project/src/hr_statistic/app "
 
         self.__located_data: list = []
 
@@ -68,7 +94,7 @@ class CleanData(LocateAndLoadData):
         super().__init__()
 
     def clean(self):
-        """### Metoda koja radi iteraciju kroz alocirane excel datoteke  i izbacuje prazna polja"""
+        """### Metoda koja radi iteraciju kroz alocirane excel datoteke  i izbacuje prazna polja i ispravlja podatke"""
         # Prljavi podatci i varijacije
         name_variant = {"Lepša": ["Lepša", "lepša ", " lepša", "lePŠA", "Lepša  ", "lepša  "],
                         "Lepša ZTP": ["lepša ZTP", "Lepša ztp "],
@@ -98,13 +124,14 @@ class CleanData(LocateAndLoadData):
         self.df.loc[:, "Datum zakazivanja"] = pd.to_datetime(self.df["Datum zakazivanja"], format="%d %B, %Y")
         
         self.df["Datum obuke"] = pd.to_datetime(self.df["Datum obuke"], format="%d %B, %Y")
-
+        
+        # Odbacivanje redova koji imaju manje od 4 vrednosti
         self.df = self.df.dropna(thresh=4)  
 
         return self.df
 
 class Analytic(CleanData):
-
+    """### Glavna klasa u kojoj se radi manipulacija nad podatcima nasledjuje klasu CleanData i ociscene podatke"""
     def __init__(self) -> None:
 
         super().__init__()
@@ -126,18 +153,28 @@ class Analytic(CleanData):
         self.df.loc[:, "Datum zakazivanja"] = pd.to_datetime(self.df["Datum zakazivanja"], format="%d %B, %Y")
         
         # Kriterijum pretrage
-        new_df = self.df.loc[(self.df["Rukovodilac"] == rukovodilac) & (self.df["Datum zakazivanja"] >= start_date) & (self.df["Datum zakazivanja"] < end_date) & (self.df["Pojavio se"].str.lower() == "da")]
+        new_df = self.df.loc[
+            (self.df["Rukovodilac"] == rukovodilac) &
+            (self.df["Datum zakazivanja"] >= start_date) & 
+            (self.df["Datum zakazivanja"] < end_date) &
+            (self.df["Pojavio se"].str.lower() == "da")
+            ]
         
         # Grupisanje podataka po "Datum zakazivanja" i brojanje za svaki dan u range
         counts_by_day = new_df.groupby("Datum zakazivanja")["Datum zakazivanja"].count()
 
-        # Preimenovanje kolona
-
         return counts_by_day
     
     def pie_data(self, start_date: str, end_date: str):
+        """### Metoda koja analizira podatke za Pie blok grafikona
+        :param
+        - `start_date: str` -> Datum od: pocetak range-a
+        - `end_date: str` -> Datum do: kraj range-a
+
+        :return
+        - `tuple[dict, dict..]` -> Vraca cetiri dict-a u tuple-u koji predstavljaju podatke za pie graph
+        """
         # Formatiranje datuma
-        
         start_date = pd.to_datetime(start_date, format="%d-%m-%Y")
         end_date = pd.to_datetime(end_date, format="%d-%m-%Y")
 
@@ -196,7 +233,8 @@ class Analytic(CleanData):
             ukupan_broj_obuka = len(new_df_traning)
 
             ukupan_broj_declined = len(new_df_traning_declined)
-
+            
+            # Nesme da se prikaze 0
             if ukupan_broj_intervjua > 0 and ukupan_broj_primljenih > 0 and ukupan_broj_obuka > 0 and ukupan_broj_declined > 0:
                 
                 rezultati[rukovodilac] = ukupan_broj_intervjua
@@ -209,14 +247,92 @@ class Analytic(CleanData):
 
         return rezultati, rezultati_acepted, rezultati_obuka, rezultati_declined
     
+    def refuse_messages_data(self, start_date: str, end_date: str):
+        """### Metoda koja analizira podatke za Dash tabel
+        :param
+        - `start_date: str` -> Datum od: pocetak range-a
+        - `end_date: str` -> Datum do: kraj range-a
+
+        :return
+        - `tuple[list[dict[str, Any]], list]` -> Vraca kompleksnu strukturu tabel_list koju zahteva tooltip
+        za kreiranje markdown tabele, unique_messages_to_dict prikaz poruka
+        """
+        # Formatiranje datuma
+        start_date = pd.to_datetime(start_date, format="%d-%m-%Y")
+        end_date = pd.to_datetime(end_date, format="%d-%m-%Y")
+
+        # Konvertujemo sve u str cak i nan kako bi izbegli gresku koju javlja unarni operator  ~ 
+        self.df.loc[:, "Razlog"] = self.df["Razlog"].astype(str)    
+
+        # Filtriranje podataka odbacujemo sve sto pocinje sa ~ (odusta
+        messages = self.df.loc[
+            (~self.df["Razlog"].str.startswith("(odusta")) & 
+            (self.df["Datum zakazivanja"] >= start_date) & 
+            (self.df["Datum zakazivanja"] < end_date) &
+            (self.df["Razlog"] != "nan")
+            ]
+        
+        # Gupisemo podatke i spajamo sa Broj ponavljanja
+        message_counts = messages.groupby(["Razlog"]).size().reset_index(name="Broj ponavljanja")
+
+        # Formatiranje za dash 
+        message_counts.columns = ["Razlog", "Broj ponavljanja"]
+
+        # Sortiramo podatke po Razlog-u kako bi se pogodile sa sorted_grouped_data
+        sorted_message_counts = message_counts.sort_values(by=["Razlog"])
+
+        # Kreiramo format za Dash DataTabel data=[]
+        unique_messages_to_dict = [{'Razlog': razlog, "Broj ponavljanja": broj_ponavljanja} for razlog, broj_ponavljanja in sorted_message_counts.values]
+
+        # Grupisemo rukovodilca, razlog sa Broj ponavljanja razloga
+        grouped_data = messages.groupby(["Rukovodilac", "Razlog"]).size().reset_index(name="Broj ponavljanja razloga")
+
+        # Sortiramo podatke po Razlog-u kako bi se pogodile sa sorted_message_counts
+        sorted_grouped_data = grouped_data.sort_values(by=["Razlog"])
+
+        # Lambda funkcija koja kreira dict sa brojem ponavljanja razloga za svakog Rukovodilac
+        create_rukovodioci_dict = lambda razlog: {
+            rukovodilac: broj_pojavljivanja
+            for rukovodilac, broj_pojavljivanja in zip(
+                sorted_grouped_data[sorted_grouped_data["Razlog"] == razlog]["Rukovodilac"],
+                sorted_grouped_data[sorted_grouped_data["Razlog"] == razlog]["Broj ponavljanja razloga"]
+            )
+        }
+
+        # Pozivanje lambda funkcije za svaki razlog u listi, create_rukovodioci_dict primena rezultata iz lamda
+        sorted_grouped_data["Rukovodioci dict"] = sorted_grouped_data["Razlog"].apply(create_rukovodioci_dict)
+
+        # Izbacujemo duplikate Razlog-a(Kako bi se izbeglo ponavljanje istih Rukoodilaca) i restartujemo index
+        sorted_grouped_data = sorted_grouped_data.drop_duplicates(subset="Razlog").reset_index(drop=True)
+
+        # Pomocna funkcija
+        dt = format_str(sorted_grouped_data["Rukovodioci dict"])
+
+        tabel_list = []
+
+        # Prolazimo kroz listu generisanih tabela i kreiramo format za Dash tooltip
+        for table in dt:
+
+            text_format = {
+                'Razlog': {'value': f"{table}", 'type': 'markdown'}
+            }
+
+            tabel_list.append(text_format)
+
+        return unique_messages_to_dict, tabel_list
+
         
 # l = Analytic()
 
 # l.locate_data()
 
-# l.load_data("Telemarketing")
+# l.load_data("Komercijala")
 
-# ch = l.clean()
+# l.clean()
+
+# l.refuse_messages_data("01-01-2023", "30-09-2023")
+
+# print(y)
 
 # ch = l.pie_data("01-01-2023", "30-09-2023")
 
