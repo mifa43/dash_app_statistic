e1464d7796ed784b4d3bac520495f6b7aa7f49b2
Mifa43 <mifa43kotez@gmail.com>
2023-09-11 09:26:07 +0200

    ispravljena greska u putanji. bio je jedan space dodat

--- hr_statistic/app/analytics.py
+++ hr_statistic/app/analytics.py
@@ -37,7 +37,7 @@ class LocateAndLoadData:
 
     def __init__(self) -> None:
         # local develop path ./hr_statistic/app | /opt/render/project/src/hr_statistic/app 
-        self.__path_to_data: str = "/opt/render/project/src/hr_statistic/app "
+        self.__path_to_data: str = "/opt/render/project/src/hr_statistic/app"
 
         self.__located_data: list = []
 
